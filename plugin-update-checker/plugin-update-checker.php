01a3a7dc38fc96d91c55d9e3fa76929019a4e708
bkerest <bkerest@gmail.com>
2025-07-25 16:57:50 +0300

    1

--- plugin-update-checker/plugin-update-checker.php
+++ plugin-update-checker/plugin-update-checker.php
@@ -1,2 +1,10 @@
 <?php
-require_once dirname(__FILE__) . '/Puc/v4/Factory.php';
+/**
+ * Plugin Update Checker Library 5.6
+ * http://w-shadow.com/
+ *
+ * Copyright 2025 Janis Elsts
+ * Released under the MIT license. See license.txt for details.
+ */
+
+require dirname(__FILE__) . '/load-v5p6.php';

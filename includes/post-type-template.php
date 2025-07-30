113b23cdfe86b6d59a6616aff6e875cb02ac8ca5
bkerest <bkerest@gmail.com>
2025-07-30 14:35:46 +0300

    2.1.6

--- includes/post-type-template.php
+++ includes/post-type-template.php
@@ -37,5 +37,62 @@ function wcdt_register_template_post_type() {
     );
 
     register_post_type('wc_template', $args);
+$args['supports'][] = 'page-attributes';
+
+// Add Duplicate Link
+add_filter('post_row_actions', function($actions, $post){
+    if ($post->post_type != 'wc_template') return $actions;
+    $url = wp_nonce_url(admin_url('admin.php?action=duplicate_wc_template&post=' . $post->ID), 'duplicate_wc_template_' . $post->ID);
+    $actions['duplicate'] = '<a href="' . esc_url($url) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+    return $actions;
+}, 10, 2);
+
+add_action('admin_action_duplicate_wc_template', function() {
+
+// Enable sorting by Menu Order in admin list
+add_filter('manage_edit-wc_template_sortable_columns', function($columns) {
+    $columns['menu_order'] = 'menu_order';
+    return $columns;
+});
+
+add_filter('request', function($vars) {
+    if (isset($vars['post_type']) && $vars['post_type'] == 'wc_template') {
+        if (isset($vars['orderby']) && $vars['orderby'] == 'menu_order') {
+            $vars = array_merge($vars, array(
+                'orderby' => 'menu_order'
+            ));
+        }
+    }
+    return $vars;
+});
+
+    if (empty($_GET['post']) || !current_user_can('edit_posts')) wp_die('No post to duplicate has been supplied!');
+
+    $post_id = absint($_GET['post']);
+    check_admin_referer('duplicate_wc_template_' . $post_id);
+
+    $post = get_post($post_id);
+    $new_post = array(
+        'post_title'     => 'Copy of ' . $post->post_title,
+        'post_content'   => $post->post_content,
+        'post_status'    => 'draft',
+        'post_type'      => 'wc_template',
+        'post_author'    => get_current_user_id(),
+        'menu_order'     => $post->menu_order,
+    );
+    $new_post_id = wp_insert_post($new_post);
+
+    // Copy metadata
+    $meta = get_post_meta($post_id);
+    foreach ($meta as $key => $values) {
+        foreach ($values as $value) {
+            add_post_meta($new_post_id, $key, maybe_unserialize($value));
+        }
+    }
+
+    wp_redirect(admin_url('post.php?action=edit&post=' . $new_post_id));
+    exit;
+});
+
 }
 add_action('init', 'wcdt_register_template_post_type');
